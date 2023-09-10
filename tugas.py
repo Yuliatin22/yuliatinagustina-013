a9d901e0e32be895fe5b8c91432274951c8966e9
Yuliatin22 <144197155+Yuliatin22@users.noreply.github.com>
2023-09-10 23:30:13 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,13 @@
+# mengonversi suhu celcius ke suhu fahrenheit
+
+print("mengonversi suhu celcius ke suhu fahrenheit")
+c = int(input("masukkan nilai celcius"))
+konversi = (c*9/5)+32
+print("hasil konversi suhu celcius ke suhu fahrenheit",konversi )
+
+#mengonversi suhu fahrenheit ke suhu celcius
+
+print("mengonversi suhu fahrenheit ke celcius")
+f = int(input("memasukkan nilai fahrenheit"))
+konversi = (f-32)*5/9
+print("hasil konversi suhu fahrenheit ke celcius",konversi)
